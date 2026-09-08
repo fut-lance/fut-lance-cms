901b84d263cb6694b9a7400565c01aaf2d7e8bde
Rafael Melegari de Souza <rafaelmelegari86@gmail.com>
2026-09-08 17:00:25 -0300

    Fix noticia controller - manually populate categoria

--- src/api/noticia/controllers/noticia.ts
+++ src/api/noticia/controllers/noticia.ts
@@ -2,25 +2,33 @@ import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::noticia.noticia', ({ strapi }) => ({
   async find(ctx) {
-    const { query } = ctx;
-    if (!query.populate) {
-      query.populate = { categoria: true };
-    }
-    const sanitizedQuery = await this.sanitizeQuery(ctx);
-    const results = await strapi.entityService.findMany('api::noticia.noticia', sanitizedQuery);
-    const sanitizedResults = await this.sanitizeOutput(results, ctx);
-    return this.transformResponse(sanitizedResults, {}, sanitizedQuery);
+    const sanitizer = this.sanitizeQuery;
+    const query = await sanitizer(ctx);
+    const populate = query.populate || {};
+    populate.categoria = true;
+    const results = await strapi.entityService.findMany('api::noticia.noticia', {
+      ...query,
+      populate,
+    });
+    const total = await strapi.entityService.count('api::noticia.noticia', { filters: query.filters });
+    return {
+      data: results,
+      meta: {
+        pagination: {
+          page: query.pagination?.page || 1,
+          pageSize: query.pagination?.pageSize || 25,
+          pageCount: Math.ceil(total / (query.pagination?.pageSize || 25)),
+          total,
+        },
+      },
+    };
   },
 
   async findOne(ctx) {
-    const { query } = ctx;
-    if (!query.populate) {
-      query.populate = { categoria: true };
-    }
-    const sanitizedQuery = await this.sanitizeQuery(ctx);
     const { id } = ctx.params;
-    const result = await strapi.entityService.findOne('api::noticia.noticia', id, sanitizedQuery);
-    const sanitizedResult = await this.sanitizeOutput(result, ctx);
-    return this.transformResponse(sanitizedResult, {}, sanitizedQuery);
+    const result = await strapi.entityService.findOne('api::noticia.noticia', id, {
+      populate: { categoria: true },
+    });
+    return { data: result };
   },
 }));

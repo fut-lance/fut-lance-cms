0ff2a6078937a88fd35a95294bfa3fc998627340
Rafael Melegari de Souza <rafaelmelegari86@gmail.com>
2026-09-08 16:55:37 -0300

    Fix noticia controller to populate categoria relation

--- src/api/noticia/controllers/noticia.ts
+++ src/api/noticia/controllers/noticia.ts
@@ -1,2 +1,26 @@
 import { factories } from '@strapi/strapi';
-export default factories.createCoreController('api::noticia.noticia');
+
+export default factories.createCoreController('api::noticia.noticia', ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+    if (!query.populate) {
+      query.populate = { categoria: true };
+    }
+    const sanitizedQuery = await this.sanitizeQuery(ctx);
+    const results = await strapi.entityService.findMany('api::noticia.noticia', sanitizedQuery);
+    const sanitizedResults = await this.sanitizeOutput(results, ctx);
+    return this.transformResponse(sanitizedResults, {}, sanitizedQuery);
+  },
+
+  async findOne(ctx) {
+    const { query } = ctx;
+    if (!query.populate) {
+      query.populate = { categoria: true };
+    }
+    const sanitizedQuery = await this.sanitizeQuery(ctx);
+    const { id } = ctx.params;
+    const result = await strapi.entityService.findOne('api::noticia.noticia', id, sanitizedQuery);
+    const sanitizedResult = await this.sanitizeOutput(result, ctx);
+    return this.transformResponse(sanitizedResult, {}, sanitizedQuery);
+  },
+}));
